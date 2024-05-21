7818448942a9fb1308dbd79906dac3e1155f6103
Md Asif Momin <asif.momin@geminisolutions.com>
2024-05-21 14:34:43 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-// Import modal and progress bar components
-import { updateProgress } from './components/ProgressBar/ProgressBar.js';
-import { Modal } from './components/Modal/Modal.js'; // This imports the modal component and its associated CSS and HTML files
-import TourGuide from './components/tourGuide/tourGuide.js';
-import AnnModal from './components/AnnModal/AnnModal.js';
-import { FAQComponent } from './components/Faq/faq.js';
-import { HeadingComponent } from './components/Heading/heading.js';
-import AnnouncementTicker from './components/AnnouncementTicker/AnnouncementTicker.js';
+// // Import modal and progress bar components
+// import { updateProgress } from './components/ProgressBar/ProgressBar.js';
+// import { Modal } from './components/Modal/Modal.js'; // This imports the modal component and its associated CSS and HTML files
+// import TourGuide from './components/tourGuide/tourGuide.js';
+// import AnnModal from './components/AnnModal/AnnModal.js';
+// import { FAQComponent } from './components/Faq/faq.js';
+// import { HeadingComponent } from './components/Heading/heading.js';
+// import AnnouncementTicker from './components/AnnouncementTicker/AnnouncementTicker.js';
 
-// Export components or functions as needed
-export { updateProgress, Modal, TourGuide, AnnModal, FAQComponent, HeadingComponent, AnnouncementTicker };
+// // Export components or functions as needed
+// export { updateProgress, Modal, TourGuide, AnnModal, FAQComponent, HeadingComponent, AnnouncementTicker };
+
+function square(number) {
+    return number * number;
+}
+
+module.exports = { square };
